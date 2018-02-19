ef6635f8dacf244df9ae09687751b98322df53da
Muaad <muadh24@gmail.com>
2018-02-19 17:25:20 +0300

    Mpesa icon

--- src/button.js
+++ src/button.js
@@ -2,6 +2,6 @@ var button = document.getElementById("mpesaButton");
 
 if (button !== null) {
     styles = 'background: #6AAE2D; color: white; text-align: center; font-weight: bold; padding: 15px; border-radius: 10px; font-size: 20px;'
-    markup = '<a href="" style="' + styles + '">Pay with Mpesa</a>'
+    markup = '<a href="" style="' + styles + '"><img src="../src/mpesa.png">Pay with Mpesa</a>'
     button.innerHTML = markup
 }
