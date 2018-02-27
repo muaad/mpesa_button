57a8ef89422d9e8b0f0ee3b72c0a45384c3060b3
Muaad <muadh24@gmail.com>
2018-02-27 14:16:21 +0300

    Prepopulate phone number and amount fields based on button data attributes

--- src/button.js
+++ src/button.js
@@ -5,7 +5,9 @@ if (button !== null) {
     img = '<img style="width: 35px; display: inline; margin: -8px;" src= "https://rawgit.com/muaad/mpesa_button/master/images/mpesa.png">'
     btnMarkup = '<a href="" id="mpesaBtn" class="mpesaButton">' + img + '<span style="margin-left: 15px;">Pay with Mpesa</span></a>'
     phoneInstruction = '<strong><em>We will send an Mpesa payment request to this phone number</em></stron>'
-    form = '<form><label for="phone" class="mpesaLabel">Phone Number</label><input class="mpesaInput" type="text" placeholder="254722123456" name="phone" id="mpesaPhoneNumber"></input><br>' + phoneInstruction + '<br><br><button href="" id="mpesaSend" class="mpesaButton" style="width: 100%;">' + img + '<span style="margin-left: 15px;">Pay</span></button></form>'
+    form = '<form>\
+        <label for="amount" class="mpesaLabel">Amount</label><input class="mpesaInput" type="text" placeholder="2000" name="phone" id="mpesaAmount"></input><br>\
+        <label for="phone" class="mpesaLabel">Phone Number</label><input class="mpesaInput" type="text" placeholder="254722123456" name="phone" id="mpesaPhoneNumber"></input><br>' + phoneInstruction + '<br><br><button href="" id="mpesaSend" class="mpesaButton" style="width: 100%;">' + img + '<span style="margin-left: 15px;">Pay</span></button></form>'
     formMarkup = '<div id="mpesaForm"><h3 class="mpesaHeader">Pay With Mpesa</h3>' + form + '</div>'
     button.innerHTML = btnMarkup
 
@@ -19,6 +21,12 @@ if (button !== null) {
         formDiv = document.createElement('div')
         button.parentNode.insertBefore(formDiv, button.nextSibling);
         formDiv.innerHTML = formMarkup
+        amountInput = document.getElementById("mpesaAmount")
+        phoneInput = document.getElementById("mpesaPhoneNumber")
+        phone = button.getAttribute('data-phone')
+        amount = button.getAttribute('data-amount')
+        amountInput.value = amount
+        phoneInput.value = phone
         button.style.display = 'none';
 
         payButton = document.getElementById("mpesaSend")
